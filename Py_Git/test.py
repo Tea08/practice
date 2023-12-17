0e52f2eb52d099ef037b99ae15d10aea0d3ad640
TEA <tea.kaa@ya.ru>
2023-12-17 19:20:44 +0500

    Auto commit: 2023-12-17 19:20:44

--- Py_Git/test.py
+++ Py_Git/test.py
@@ -1,41 +1,24 @@
-
 import time
 from git import Repo
 
-# Путь к директории, которую нужно отслеживать
-dir_path = 'C:/Users/TEA/code/practice'
+dir_path = 'C:/Users/TEA/code/practice'   # Путь к директории, которую нужно отслеживать  
+repo = Repo(dir_path)   # Подключение к локальному репозиторию Git
+remote_url = 'https://github.com/Tea08/practice.git'  # URL удаленного репозитория Git
 
-# Подключение к локальному репозиторию Git
-repo = Repo(dir_path)
-
-# URL удаленного репозитория Git
-remote_url = 'https://github.com/Tea08/practice.git'
-
-# Функция для проверки изменений и создания коммита
-def check_changes_and_commit():
-    # Получаем список измененных файлов
-    changed_files = [item.a_path for item in repo.index.diff(None)]
-    
-    if changed_files:
-        # Добавляем все изменения в локальный репозиторий
-        repo.index.add(changed_files)
-        
-        # Создаем коммит с автоматическим сообщением
-        commit_message = f'Auto commit: {time.strftime("%Y-%m-%d %H:%M:%S")}'
-        repo.index.commit(commit_message)
-
-        # Отправляем изменения на удаленный репозиторий
-        #remote = repo.create_remote('origin', remote_url)
-        #remote.push(refspec='main:main')
-        repo.git.push("--all", 'origin')
+def check_changes_and_commit():  # Функция для проверки изменений и создания коммита    
+    changed_files = [item.a_path for item in repo.index.diff(None)]     # Получаем список измененных файлов    
+    if changed_files:       
+        repo.index.add(changed_files)   #Добавляем все изменения в локальный репозиторий 
+        commit_message = f'Auto commit: {time.strftime("%Y-%m-%d %H:%M:%S")}'    # Создаем коммит с автоматическим сообщением
+        repo.index.commit(commit_message)        
+        repo.git.push("--all", 'origin')     # Отправляем изменения на удаленный репозиторий
         print('Изменения успешно отправлены на удаленный репозиторий.')
     else:
         print('Изменений не обнаружено.')
 
-# Периодически проверяем изменения раз в час
-while True:
+while True:    # Периодически проверяем изменения раз в час
     check_changes_and_commit()
-    time.sleep(60)  # 3600 секунд = 1 час
+    time.sleep(3600)  # 3600 секунд = 1 час
 
 if __name__ == "__main__":
     main()
