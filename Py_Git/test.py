8b32a3dcde1d12b3834b188a6633a2a5d997c856
TEA <tea.kaa@ya.ru>
2023-12-17 19:35:07 +0500

    Auto commit: 2023-12-17 19:35:07

--- Py_Git/test.py
+++ Py_Git/test.py
@@ -8,7 +8,7 @@ remote_url = 'https://github.com/Tea08/practice.git'  # URL удаленного
 def check_changes_and_commit():  # Функция для проверки изменений и создания коммита    
     changed_files = [item.a_path for item in repo.index.diff(None)]     # Получаем список измененных файлов    
     if changed_files:       
-        repo.index.add(changed_files)   #Добавляем все изменения в локальный репозиторий 
+        repo.index.add(changed_files)   # Добавляем все изменения в локальный репозиторий 
         commit_message = f'Auto commit: {time.strftime("%Y-%m-%d %H:%M:%S")}'    # Создаем коммит с автоматическим сообщением
         repo.index.commit(commit_message)        
         repo.git.push("--all", 'origin')     # Отправляем изменения на удаленный репозиторий
